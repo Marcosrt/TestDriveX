a1860f608cd7a7f3860cc41f20b8454edcd3cf6f
Marcos Roberto Tetzlaff <marcosrt22@hotmail.com>
2017-09-06 20:17:22 -0300

    Finalizando TestDriveX

--- TestDrive/TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
+++ TestDrive/TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
@@ -12,9 +12,27 @@ namespace TestDrive.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AgendamentoView : ContentPage
 	{
-		public AgendamentoView ()
+        public Veiculo Veiculo { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Fone { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DataAgendamento { get; set; }
+
+        public TimeSpan HoraAgendamento { get; set; }
+        public AgendamentoView (Veiculo veiculo)
 		{
+            this.Veiculo = veiculo;
 			InitializeComponent ();
+            this.BindingContext = this;
 		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Salvar Agendamento", "Nome: " + Nome, "Ok");
+        }
 	}
 }
